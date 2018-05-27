e7c3b23b20d597ece9632974513f8a58382b8cb3
Cosme Domínguez Díaz <cosme.ddiaz@gmail.com>
2018-05-27 02:07:50 +0200

    msm8996-common: Remove privapp-permissions-oem.xml and move qti's xml to /system
      * privapp-permissions-qti.xml does the same and more.
    
      * Move back qti_whitelist.xml and privapp-permissions-qti.xml
      to /system as it's the right place:
    
      https://source.android.com/devices/tech/config/perms-whitelist#adding-whitelists
    
    Signed-off-by: mracar07 <mr.acar07@gmail.com>

--- msm8996.mk
+++ msm8996.mk
@@ -244,14 +244,8 @@ PRODUCT_PACKAGES += \
 
 # QCOM
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/privapp-permissions-qti.xml
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
-
-# Qualcomm
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/privapp-permissions-oem.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/privapp-permissions-oem.xml
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml \
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
 
 # QMI
 PRODUCT_PACKAGES += \
