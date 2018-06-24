666352b1815fb2e4a84e672264c69ce469c43595
davidevinavil <davidevinavil@gmail.com>
2018-06-24 23:47:02 +0200

    msm8996-common: Drop privapp-permissions-oem.xml

--- msm8996.mk
+++ msm8996.mk
@@ -249,10 +249,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
-# Qualcomm
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/privapp-permissions-oem.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/privapp-permissions-oem.xml
-
 # QMI
 PRODUCT_PACKAGES += \
     libjson
