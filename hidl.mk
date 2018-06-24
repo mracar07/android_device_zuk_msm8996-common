310b002c92fe3fc7974c58f18f4f7d151e5506d8
davidevinavil <davidevinavil@gmail.com>
2018-06-24 23:53:42 +0200

    msm8996-common: Add some camera hal

--- hidl.mk
+++ hidl.mk
@@ -38,7 +38,10 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service
+    android.hardware.camera.provider@2.4-service \
+    camera.device@3.2-impl \
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0_vendor
 
 # Display
 PRODUCT_PACKAGES += \
